3666caec4d8ce4871dae03cb78788b9afdf34468
NHTanvir <naymulhasantanvir10@gmail.com>
2024-05-09 21:58:21 +0600

    plugin renamed and some basic folder for strcuture added

---
+++ light-commerce.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: LightCommerce
+ * Plugin URI: https://naymul.com
+ * Description: Lighter ecommerce plugin 
+ * Version: 0.9
+ * Author: Naymul Hasan tanvir
+ * Author URI: https://naymul.com
+ * License: GPL2
+ */
