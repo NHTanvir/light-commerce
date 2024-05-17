1ee5d4906818c15184c2a7f636683603c9ba0b9f
NHTanvir <naymulhasantanvir10@gmail.com>
2024-05-17 19:58:56 +0600

    database class add remove method added and order class code added

--- src/common/classes/order.php
+++ src/common/classes/order.php
@@ -1,1 +1,80 @@
-order.php
+<?php
+namespace LightCommerce;
+
+use LightCommerce\Admin\Database;
+
+class Order {
+    private $id;
+    private $customer_name;
+    private $total_amount;
+    private $db;
+
+    public function __construct($id = null) {
+        $this->db = new Database();
+        if ($id) {
+            $this->id = $id;
+            $this->load_order($id);
+        }
+    }
+
+    private function load_order($id) {
+        global $wpdb;
+        $table_name = $wpdb->prefix . 'lightcommerce_order';
+        $order = $wpdb->get_row($wpdb->prepare("SELECT * FROM $table_name WHERE id = %d", $id));
+
+        if ($order) {
+            $this->customer_name = $order->customer_name;
+            $this->total_amount = $order->total_amount;
+        }
+    }
+
+    public function get_id() {
+        return $this->id;
+    }
+
+    public function get_customer_name() {
+        return $this->customer_name;
+    }
+
+    public function get_total_amount() {
+        return $this->total_amount;
+    }
+
+    public function set_customer_name($customer_name) {
+        $this->customer_name = $customer_name;
+    }
+
+    public function set_total_amount($total_amount) {
+        $this->total_amount = $total_amount;
+    }
+
+    public function add_order($customer_name, $total_amount) {
+        $this->id = $this->db->add_order($customer_name, $total_amount);
+        if ($this->id) {
+            $this->customer_name = $customer_name;
+            $this->total_amount = $total_amount;
+            return $this->id;
+        }
+        return false;
+    }
+
+    public function update_order() {
+        if ($this->id) {
+            return $this->db->update_order($this->id, $this->customer_name, $this->total_amount);
+        }
+        return false;
+    }
+
+    public function delete_order() {
+        if ($this->id) {
+            $result = $this->db->delete_order($this->id);
+            if ($result) {
+                $this->id = null;
+                $this->customer_name = null;
+                $this->total_amount = null;
+                return true;
+            }
+        }
+        return false;
+    }
+}
--- src/common/database/Database.php
+++ src/common/database/Database.php
@@ -71,4 +71,168 @@ class Database {
         require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
         dbDelta($sql);
     }
+
+    // Add a new product
+    public function add_product($name, $price, $description = '') {
+        $this->wpdb->insert(
+            $this->wpdb->prefix . 'lightcommerce_product',
+            [
+                'name' => $name,
+                'price' => $price,
+                'description' => $description
+            ],
+            [
+                '%s', '%f', '%s'
+            ]
+        );
+        return $this->wpdb->insert_id;
+    }
+
+    // Update an existing product
+    public function update_product($id, $name, $price, $description = '') {
+        return $this->wpdb->update(
+            $this->wpdb->prefix . 'lightcommerce_product',
+            [
+                'name' => $name,
+                'price' => $price,
+                'description' => $description
+            ],
+            ['id' => $id],
+            [
+                '%s', '%f', '%s'
+            ],
+            ['%d']
+        );
+    }
+
+    // Delete a product
+    public function delete_product($id) {
+        return $this->wpdb->delete(
+            $this->wpdb->prefix . 'lightcommerce_product',
+            ['id' => $id],
+            ['%d']
+        );
+    }
+
+    // Add a new product meta
+    public function add_product_meta($product_id, $meta_key, $meta_value) {
+        $this->wpdb->insert(
+            $this->wpdb->prefix . 'lightcommerce_product_meta',
+            [
+                'product_id' => $product_id,
+                'meta_key' => $meta_key,
+                'meta_value' => $meta_value
+            ],
+            [
+                '%d', '%s', '%s'
+            ]
+        );
+        return $this->wpdb->insert_id;
+    }
+
+    // Update an existing product meta
+    public function update_product_meta($id, $meta_key, $meta_value) {
+        return $this->wpdb->update(
+            $this->wpdb->prefix . 'lightcommerce_product_meta',
+            [
+                'meta_key' => $meta_key,
+                'meta_value' => $meta_value
+            ],
+            ['id' => $id],
+            [
+                '%s', '%s'
+            ],
+            ['%d']
+        );
+    }
+
+    // Delete a product meta
+    public function delete_product_meta($id) {
+        return $this->wpdb->delete(
+            $this->wpdb->prefix . 'lightcommerce_product_meta',
+            ['id' => $id],
+            ['%d']
+        );
+    }
+
+    // Add a new order
+    public function add_order($customer_name, $total_amount) {
+        $this->wpdb->insert(
+            $this->wpdb->prefix . 'lightcommerce_order',
+            [
+                'customer_name' => $customer_name,
+                'total_amount' => $total_amount
+            ],
+            [
+                '%s', '%f'
+            ]
+        );
+        return $this->wpdb->insert_id;
+    }
+
+    // Update an existing order
+    public function update_order($id, $customer_name, $total_amount) {
+        return $this->wpdb->update(
+            $this->wpdb->prefix . 'lightcommerce_order',
+            [
+                'customer_name' => $customer_name,
+                'total_amount' => $total_amount
+            ],
+            ['id' => $id],
+            [
+                '%s', '%f'
+            ],
+            ['%d']
+        );
+    }
+
+    // Delete an order
+    public function delete_order($id) {
+        return $this->wpdb->delete(
+            $this->wpdb->prefix . 'lightcommerce_order',
+            ['id' => $id],
+            ['%d']
+        );
+    }
+
+    // Add a new order meta
+    public function add_order_meta($order_id, $meta_key, $meta_value) {
+        $this->wpdb->insert(
+            $this->wpdb->prefix . 'lightcommerce_order_meta',
+            [
+                'order_id' => $order_id,
+                'meta_key' => $meta_key,
+                'meta_value' => $meta_value
+            ],
+            [
+                '%d', '%s', '%s'
+            ]
+        );
+        return $this->wpdb->insert_id;
+    }
+
+    // Update an existing order meta
+    public function update_order_meta($id, $meta_key, $meta_value) {
+        return $this->wpdb->update(
+            $this->wpdb->prefix . 'lightcommerce_order_meta',
+            [
+                'meta_key' => $meta_key,
+                'meta_value' => $meta_value
+            ],
+            ['id' => $id],
+            [
+                '%s', '%s'
+            ],
+            ['%d']
+        );
+    }
+
+    // Delete an order meta
+    public function delete_order_meta($id) {
+        return $this->wpdb->delete(
+            $this->wpdb->prefix . 'lightcommerce_order_meta',
+            ['id' => $id],
+            ['%d']
+        );
+    }
 }
