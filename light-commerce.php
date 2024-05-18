bf685bf362e11d2487a42c37909e460c1637a419
NHTanvir <naymulhasantanvir10@gmail.com>
2024-05-18 22:32:53 +0600

    Use the installer and initial seeding to create dummy porduct wokring

--- light-commerce.php
+++ light-commerce.php
@@ -25,13 +25,33 @@ class ClassLoader{
         new Front\Shortcode();
         new Front\Cart();
         new Front\Checkout();
+        register_activation_hook(__FILE__, array($this, 'initialize_dummy_products'));
+    }
+
+    public function initialize_dummy_products() {
+
+        if(  get_option( 'lightcommerce_dummy_products_initialized' ) ) return;
+
+        $db = new Admin\Database();
+        $dummy_products = array(
+            array('name' => 'Product 1', 'price' => 10.99, 'description' => 'Description for Product 1'),
+            array('name' => 'Product 2', 'price' => 19.99, 'description' => 'Description for Product 2'),
+
+        );
+
+        foreach ($dummy_products as $product) {
+            $db->add_product( $product['name'], $product['price'] , $product['description'] );
+        }
+        
+        update_option( 'lightcommerce_dummy_products_initialized', true );
+
     }
 }
 new ClassLoader;
 
-
 define('LIGHT_COMMERCE_VERSION', '1.0.0');
 define('LIGHT_COMMERCE_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('LIGHT_COMMERCE_PLUGIN_URL', plugin_dir_url(__FILE__));
+
 
 
